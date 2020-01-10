a0f7447c50af9ae5380cf5f5ecffa7a25e9bab16
gerardnico <gerardnico@gmail.com>
2020-01-10 17:21:41 +0100

    Setting the domain

--- javascripts/set_cookie_example.js
+++ javascripts/set_cookie_example.js
@@ -1,7 +1,7 @@
 // use in the page https://gerardnico.com/web/http/cookie_domain
 
 // Setting the cookie
-document.cookie="cookieGitHub=valueFromGithub";
+document.cookie="cookieGitHub=valueFromGithub;domain=gerardnico.github.io";
 
 // A console
 console.log('The cookie from Git hub was set');
